63b2be40cb65642e67aaa6a9e0e5e97f066ece06
maremarismaria <mariamoralespadron@gmail.com>
2018-02-10 20:40:44 +0000

    testing directives

--- app.js
+++ app.js
@@ -4,11 +4,30 @@
     var app = angular.module('taskApp', []);
 
     app.controller('mainController', function($scope, $http){
-        
+        /*
         $http.get("app-data/data.json").then(function(response) {
             $scope.data = response.data;
         });
 
+        
+        $scope.visibility = false;
+        $scope.showButton = function() {
+            $scope.visibility = !$scope.visibility;
+        }
+        */
+
     });
 
+    app.directive("userDetails", function(){
+        return {
+            restrict : 'E',
+            templateUrl : "user-details.html",
+            controller: function($scope, $http){
+                $http.get("app-data/data.json").then(function(response) {
+                    $scope.data = response.data;
+                });
+            }
+        };
+    });
+    
 })();
